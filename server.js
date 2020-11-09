57c3b56b2a46d105aa5f2b80f398b104a61ce5ff
ilyakolker <ilyakolker@gmail.com>
2020-11-09 10:51:19 +0200

    last push

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const players_list = require('./data/players.json')
 const app = express();
 app.use(express.json());
 
- const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
  
 let add_player_to_team = (all_ranks) => {
   let team1 = [];
